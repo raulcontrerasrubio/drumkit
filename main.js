31ef539b1ced8d89a0ed12655ed14e33f7244e41
Raul Contreras <raulc98@gmail.com>
2019-02-27 12:43:13 +0100

    Now you can play a sound clicking on the box

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 window.onload = (function (){
   document.addEventListener('keydown', keyPress);
   document.addEventListener('keyup', keyRelease);
+  handleOnClickEvent();
 
   function keyPress(evt){
     let box = document.querySelector(`.box[data-key="${evt.keyCode}"]`);
     let audio = document.querySelector(`audio[data-key="${evt.keyCode}"]`);
-    
-    if(!(box || audio)){return;}
-    
     box.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+    play(box, audio);
   }
 
   function keyRelease(evt){
     let box = document.querySelector(`.box[data-key="${evt.keyCode}"]`);
-    if(!box){
-      return;
-    }
-    
-    box.classList.remove('playing');
+    if(!box){ return; }
 
+    box.classList.remove('playing');
+  }
+
+  function handleOnClickEvent(){
+    var buttons = document.querySelectorAll('.box[data-key]');
+    for(let btn of buttons){
+      btn.onclick = function(){
+        let code = this.getAttribute('data-key');
+        let audio = document.querySelector(`audio[data-key="${code}"]`)
+        
+        play(this, audio);
+      }
+    }
+  }
+
+  function play(box, audio){
+    if(!(box || audio)){return;}
+
+    audio.currentTime = 0;
+    audio.play();
   }
 
   
